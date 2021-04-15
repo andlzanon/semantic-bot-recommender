3c8e0d84a4f5d24137e00b0264e97ff802be6195
Andre Zanon <andrelzanon10@gmail.com>
2021-04-15 11:23:25 -0300

    Project source code commit

---
+++ SemanticBot/main.py
@@ -0,0 +1,218 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import entropy
+from random import seed
+from random import randint
+
+
+def tf_idf(sub_graph: pd.DataFrame, full_graph: pd.DataFrame, prop: str, obj: str):
+    """
+    Function that returns the tf-idf of property and value tuple
+    :param full_graph: full graph with all movies properties from wikidata
+    :param prop: property to value tf-idf
+    :param obj: value to value tf-idf
+    :return: float tf-idf of the property (e.g. Woody Allen as a director)
+    """
+    tf = len(sub_graph.loc[(sub_graph['prop'] == prop) & (sub_graph['obj'] == obj)])
+    n = len(full_graph.index.unique())
+    idf = np.log((n/tf))
+
+    tfidf = tf * idf
+
+    return tfidf
+
+
+def prop_most_pop(sub_graph: pd.DataFrame, prop: str):
+    """
+    Function that returns the most popular values for property prop
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :param prop: property the user is looking for
+    :return: list of the ten most popular values of property
+    """
+    return sub_graph[(sub_graph['prop'] == prop)]['obj'].value_counts().index.values[:10]
+
+
+def calculate_entropy(sub_graph: pd.DataFrame):
+    """
+    Function that calculates the entropy for all properties
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :return: entropies of properties on dictionary
+    """
+    entropies = {}
+    for prop in sub_graph['prop'].unique():
+        values = sub_graph[(sub_graph['prop'] == prop)]['obj'].unique()
+        denom = len(sub_graph[(sub_graph['prop'] == prop)]['obj'])
+        prob = []
+        for value in values:
+            num = len(sub_graph.loc[(sub_graph['prop'] == prop) & (sub_graph['obj'] == value)])
+            prob.append(num/denom)
+
+        entropies[prop] = entropy(prob, base=2)
+
+    return entropies
+
+
+def shrink_graph(sub_graph: pd.DataFrame, prop: str, obj: str):
+    """
+    Function that shrinks the graph to a sub graph based on the property and value passed on the parameters.
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :param prop: property the user is looking for
+    :param obj: value of property the user is looking for
+    :return: shirked graph of the one passed as parameter
+    """
+    shrinked = sub_graph.loc[(sub_graph['prop'] == prop) & (sub_graph['obj'] == obj)]
+
+    for movie_id in shrinked.index.unique():
+        shrinked = pd.concat([shrinked, sub_graph.loc[movie_id]])
+
+    shrinked = shrinked.loc[(shrinked['prop'] != prop) & (shrinked['obj'] != obj)]
+    return shrinked.sort_index()
+
+
+def order_movies(sub_graph: pd.DataFrame, ratings: pd.DataFrame):
+    """
+    Function that order the movies based on its' popularity
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :param ratings: ratings dataset in the format user_id, movie_id, rating
+    :return: ordered movies on a DataFrame
+    """
+    ordered_movies = pd.DataFrame(index=sub_graph.index.unique(), columns=['value'])
+    for m in sub_graph.index.unique():
+        ordered_movies.loc[m] = len(ratings[(ratings['movie_id'] == m)])
+
+    return ordered_movies.sort_values(by=['value'], ascending=False)
+
+
+def order_props(sub_graph: pd.DataFrame,  full_graph: pd.DataFrame):
+    """
+    Function that order the properties based on the entropy of the property and the relevance of the value
+    measured by the tf-idf metric
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :param full_graph: full graph with all movies properties from wikidata
+    :return: ordered properties on a DataFrame
+    """
+    ordered_properties = sub_graph.loc[(sub_graph['prop'] != 'director') & (sub_graph['obj'] != 'Woody Allen')][['prop', 'obj']]
+    ordered_properties = ordered_properties.drop_duplicates()
+
+    entrs = calculate_entropy(sub_graph)
+    for index, row in ordered_properties.iterrows():
+        prop = row[0]
+        obj = row[1]
+        h = entrs[prop]
+        rel = tf_idf(sub_graph, full_graph, prop, obj)
+        ordered_properties.loc[(ordered_properties.index == index) & (ordered_properties['prop'] == prop) &
+                               (ordered_properties['obj'] == obj), 'entropy'] = h
+        ordered_properties.loc[(ordered_properties.index == index) & (ordered_properties['prop'] == prop) &
+                               (ordered_properties['obj'] == obj), 'tf_idf'] = rel
+        ordered_properties.loc[(ordered_properties.index == index) & (ordered_properties['prop'] == prop) &
+                               (ordered_properties['obj'] == obj), 'value'] = rel * h
+
+    return ordered_properties.sort_values(by=['value'], ascending=False)
+
+
+def order_props_and_movies(sub_graph: pd.DataFrame, full_graph: pd.DataFrame, ratings: pd.DataFrame):
+    """
+    Function that orders properties and movies based on user choices
+    :param sub_graph: sub graph that represents the current graph that matches the users preferences
+    :param full_graph: full graph with all movies properties from wikidata
+    :param ratings: ratings dataset in the format user_id, movie_id, rating
+    :return: the ordered movies and properties DataFrames
+    """
+    ordered_movies = order_movies(sub_graph, ratings)
+    ordered_props = order_props(sub_graph, full_graph)
+
+    return ordered_movies, ordered_props
+
+
+# import database and import of the ratings
+prop_df = pd.read_csv("../WikidataIntegration/wikidata_integration_small.csv")
+prop_df = prop_df.set_index('movie_id')
+
+ratings = pd.read_csv("../Base de Dados HetRec Arpit/1851_movies_ratings.txt", sep='\t', header=None)
+ratings.columns = ['user_id', 'movie_id', 'rating']
+
+# copy original property graph to shrink it
+sub_graph = prop_df.copy()
+
+# start conversation
+print("Hello, I'm here to help you choose a movie. We have these characteristics: \n")
+print(*sub_graph['prop'].unique(), sep="\n", end="\n\n")
+print("From which one are you interested in exploring today?")
+
+# set end conversation to false to end the talk when movie rec is accepted
+end_conversation = False
+
+# ask user for fav prop and value and then shrink graph
+p_chosen = str(input())
+print("These are the favorites along the characteristic:")
+print(*prop_most_pop(sub_graph, p_chosen),sep="\n", end="\n\n")
+print("Which one are you looking for in one of these?")
+o_chosen = str(input())
+
+watched = []
+seed(42)
+# start the loop until the recommendation is accepted or there are no movies based on users' filters
+while not end_conversation:
+
+    # get subgraph based on property chosen and order properties
+    sub_graph = shrink_graph(sub_graph, p_chosen, o_chosen)
+    top_m, top_p = order_props_and_movies(sub_graph, prop_df, ratings)
+
+    resp = "no"
+    prop_index = 0
+    rec_index = 0
+
+    # while user did not like recommendation or property suggestion do not shrink graph again
+    while resp == "no" or resp == "watched":
+        # choose action and ask if user liked it
+        ask = randint(0, 10) % 2
+
+        # if sub graph is empty there are no entries or there are no movies, recommendation fails
+        if len(sub_graph) == 0 or len(top_m) == 0 or len(top_p) == 0:
+            end_conversation = True
+            break
+
+        # if ask == 0 suggest new property
+        if ask == 0:
+            # show most relevant property
+            p_chosen = str(top_p.iloc[prop_index]['prop'])
+            o_chosen = str(top_p.iloc[prop_index]['obj'])
+            print("Do you like or not the " + p_chosen + " " + o_chosen + "? (yes/no)")
+
+            # if did not like prop try next one
+            prop_index = prop_index + 1
+            resp = str(input())
+
+        # if ask != 0 recommend movie
+        else:
+            print("Based on your current preferences, this movie may be suited for you: ")
+
+            # get movie not seen by user
+            w = False
+            while (not w) and (rec_index < len(top_m.index)):
+                if top_m.index[rec_index] in watched:
+                    rec_index = rec_index + 1
+                else:
+                    w = True
+
+            # case if all movies with properties were recommended but no movies were accepted by user
+            if rec_index == len(top_m.index):
+                print("You have already watched all the movies with the properties you liked :(")
+                end_conversation = True
+                break
+
+            # show recommendation
+            print(prop_df.loc[top_m.index[rec_index]]['title'].unique()[0])
+            print("Did you like the recommendation, didn't like the recommendation or have you "
+                  "already watched the movie? (yes/no/watched)")
+
+            # hear answer
+            resp = str(input())
+
+            # if liked the recommendation end conversation
+            if resp == "yes":
+                print("Have a good time watching the movie " + prop_df.loc[top_m.index[rec_index]]['title'].unique()[0] +
+                      ". Please come again!")
+                end_conversation = True
+            else:
+                watched.append(top_m.index[rec_index])
---
+++ WikidataIntegration/main.py
@@ -0,0 +1,117 @@
+import pandas as pd
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+def get_movie_data_from_wikidata(slice_movie_set: pd.DataFrame):
+    """
+    Function that consults the wikidata KG for a slice of the movies set
+    :param slice_movie_set: slice of the movie data set with movie id as index and imdbId, Title, year and imdbUrl
+    as columns
+    :return: JSON with the results of the query
+    """
+    imdbIdList = slice_movie_set['full_imdbId'].to_list()
+
+    imdbs = ""
+    for i in range(0, len(imdbIdList)):
+        imdbId = imdbIdList[i]
+        imdbs += " ""\"""" + imdbId + """\" """
+
+    endpoint_url = "https://query.wikidata.org/sparql"
+
+    query = """
+    SELECT DISTINCT
+          ?itemLabel
+          ?propertyItemLabel
+          ?valueLabel ?imdbId
+        WHERE 
+        {
+          ?item wdt:P345 ?imdbId .
+          ?item ?propertyRel ?value.
+          VALUES ?imdbId {""" + imdbs + """} .
+          ?propertyItem wikibase:directClaim ?propertyRel .
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". } .
+          FILTER( 
+            ?propertyRel = wdt:P179 || 
+            ?propertyRel = wdt:P136 || ?propertyRel = wdt:P170 || 
+            ?propertyRel = wdt:P57 || ?propertyRel = wdt:P58 || ?propertyRel = wdt:P161 ||
+            ?propertyRel = wdt:P725 ||  ?propertyRel = wdt:P1040 ||
+            ?propertyRel = wdt:P86 || ?propertyRel = wdt:P162 ||  ?propertyRel = wdt:P272 || 
+            ?propertyRel = wdt:P344 || ?propertyRel = wdt:P166 || ?propertyRel = wdt:P1411 || 
+            ?propertyRel = wdt:P2515 ||
+            ?propertyRel = wdt:P921 || ?propertyRel = wdt:P175
+          )  
+        }
+        ORDER BY ?imdbId"""
+
+    user_agent = "WikidatabotIntegration/1.0 https://www.wikidata.org/wiki/User:Andrelzan) " \
+                 "wiki-bot-integration/1.0"
+
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    results_dic = results_to_dict(slice_movie_set, results)
+    return results_dic
+
+
+def results_to_dict(slice_movie_set: pd.DataFrame, props_movie: dict):
+    """
+    Function that returns vector of dictionaries with the results from the dbpedia to insert into data frame of all
+    movie properties
+    :param slice_movie_set: slice of the movie data set with movie id as index and imdbId, Title, year and imdbUrl
+    as columns
+    :param props_movie: results of the returned result from the wikidata query
+    :return: vector of dictionaries of the results of the wikidata query that can be appendable to a pandas df
+    """
+
+    # change the index to the imdbId in order to add the movie_id based on the imdb latter
+    slice_movie_set.reset_index(level=0, inplace=True)
+    slice_movie_set = slice_movie_set.set_index("full_imdbId")
+
+    filter_props = []
+    for line in props_movie["results"]["bindings"]:
+        m_title = line["itemLabel"]["value"]
+        m_prop = line["propertyItemLabel"]["value"]
+        m_obj = line["valueLabel"]["value"]
+        m_imdb = line["imdbId"]["value"]
+
+        dict_props = {"movie_id": slice_movie_set.loc[m_imdb, 'movie_id'], "imdbId": m_imdb, "title": m_title, "prop": m_prop,
+                      "obj": m_obj}
+        filter_props.append(dict_props)
+
+    return filter_props
+
+
+# read movies set dataset, remove moives with nan imdbLink, set index to movie id and sort by the
+# full imdbid that matches with the wikidata format "ttXXXXXXX"
+movies_set = pd.read_csv("../../Base_Arpit/Base de Dados HetRec Arpit/Items - hetrec_after_2000.dat", "\t",
+                         header=None)
+s_all_movies = len(movies_set)
+movies_set.columns = ['movie_id', 'imdbId', 'title', 'year', 'imdbLink']
+movies_set = movies_set[movies_set['imdbLink'].notnull()]
+movies_set = movies_set.set_index('movie_id')
+movies_set['full_imdbId'] = movies_set['imdbLink'].apply(lambda x: x.split("/")[-2])
+movies_set = movies_set.sort_values(by=['full_imdbId'])
+
+# create output, final dataframe with all properties of movies
+all_movie_props = pd.DataFrame(columns=['movie_id', 'title', 'prop', 'obj'])
+
+# obtaind properties of movies in 300 movies batches
+begin = 0
+end = 350
+total = len(movies_set)
+
+print("Start obtaining movie data")
+while end <= total:
+    results = get_movie_data_from_wikidata(movies_set.iloc[begin:end])
+    all_movie_props = all_movie_props.append(results)
+    print("From " + str(begin) + " to " + str(end-1) + " obtained from Wikidata")
+    begin = end
+    end = end + 300
+print("End obtaining movie data")
+
+# save output
+all_movie_props.to_csv("./wikidata_integration_small.csv", mode='w', header=True, index=False)
+print("Coverage: " + str(len(all_movie_props['movie_id'].unique())) + " obtained of " + str(s_all_movies)
+      + ". Percentage: " + str(len(all_movie_props['movie_id'].unique())/s_all_movies))
+print('Output file generated')
